793876282f6f89cf0fd2fa20b990cf46b3fbc990
kim <kshf59@naver.com>
2017-11-22 19:00:25 +0900

    '

--- chatbot/rest-api/openapi.js
+++ chatbot/rest-api/openapi.js
@@ -1,24 +1,35 @@
-const request = require('request')
+const request = require('request');
 
 const searchNewAddress = (searchWord) => {
-    request({
-        uri: 'http://openapi.epost.go.kr/postal/retrieveNewAdressAreaCdService/retrieveNewAdressAreaCdService/getNewAddressListAreaCd',
-        qs: {
-            'ServiceKey': '2IpWxjQOHVloVq5KV1aagDyehbUJhN8KzVlF2R875LOvUUCDbleSzmLUt2dApjO8l3OpmcJxo6tdgKjQr4O8KQ%3D%3D',
-            'searchSe': 'dong',
-            'srchwrd': encodeURIComponent('searchWord'),
-            'countPerPage': '10',
-            'currentPage': '1'
-        },
-        method: 'GET',
+    var uri = 'http://openapi.epost.go.kr/postal/retrieveNewAdressAreaCdService/retrieveNewAdressAreaCdService/getNewAddressListAreaCd';
+    /* Service Key*/
+    var queryString = '?ServiceKey=' + '2IpWxjQOHVloVq5KV1aagDyehbUJhN8KzVlF2R875LOvUUCDbleSzmLUt2dApjO8l3OpmcJxo6tdgKjQr4O8KQ%3D%3D';
 
-    }, function(error, response, body) {
-        console.log('=>Status', response.statusCode);
-        console.log('=>Headers', JSON.stringify(response.headers));
-        console.log('=>Reponse received', body);
-    });
+    /* dong : 동(읍/면)명 road :도로명[default] post : 우편번호 */
+    queryString += '&searchSe=road';
+
+    /* 검색어 */
+    queryString += '&srchwrd=' + encodeURIComponent(searchWord); 
+
+    /* 페이지당 출력될 개수를 지정 */
+    queryString += '&currentPerPage=10';
+
+    /* 출력될 페이지 번호 */
+    queryString += '&currentPage=1';
+
+    request({
+        uri: uri + queryString,
+    }, function (error, response, body) {
+        console.log('=> Status', response.statusCode);
+        console.log('=> Headers', JSON.stringify(response.headers));
+        console.log('=> Reponse received', body);
+    });  
 };
 
+searchNewAddress('금화로82번길 17');
+
+/*
 module.exports = {
     searchNewAddress
 };
+*/
