88251f012d33e6952bc73c1ca0a5b693d4d13807
kim <kshf59@naver.com>
2017-12-11 21:01:22 +0900

    d

--- iot-py/pub-sub.py
+++ iot-py/pub-sub.py
@@ -7,7 +7,7 @@ import json
 
 def customCallback(client, userdata, message):
     print("메시지 수신: \n")
-    print("사서함 ㅣ름: \n")
+    print("사서함 이름: \n")
     print(message.topic + "\n")
     print("메시지 사용: \n")
     print(message.payload + "\n")
@@ -17,6 +17,30 @@ host = a3w0dqjvzngs31.iot.ap-northeast-2.amazonaws.com
 rootCAPath = "root-CA.crt"
 certificatePath = "dev01.cert.pem"
 privateKeyPath = "dev01.private.key"
-useWebsocket = NONE
+useWebsocket = FALSE
 clientId = "kim02"
 topic = "topic_1"
+
+logger = logging.getLogger("AWSIoTPythonSDK.core")
+logger.setLevel(logging.DEBUG)
+streamHandler = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+streamHandler.setFormatter(formatter)
+logger.addHandler(streamHandler)
+
+myAWSIoTMQTTClient = AWSIoTMQTTClient(clientId)
+myAWSIoTMQTTClient.configureEndpoint(host, 8883)
+myAWSIoTMQTTClient.configureCredentials(rootCAPath, privateKeyPath, certificatePath)
+
+# AWSIoTMQTTClient connection configuration
+myAWSIoTMQTTClient.configureAutoReconnectBackoffTime(1, 32, 20)
+myAWSIoTMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
+myAWSIoTMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
+myAWSIoTMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
+myAWSIoTMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
+
+
+myAWSIoTMQTTClient.connect()
+print("connect\n")
+
+myAWSIoTMQTTClient.subscribe(topic, 1, customCallback)
