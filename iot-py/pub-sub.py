bcfbd007c60ced3744c02b5b24aef67bff150d94
kim <kshf59@naver.com>
2017-12-12 19:23:59 +0900

    d

--- iot-py/pub-sub.py
+++ iot-py/pub-sub.py
@@ -17,7 +17,7 @@ def customCallback(client, userdata, message):
     ledState = dict['led']
     if ledState == "on":
         led.onLed(True)
-    else 
+    else:
         led.onLed(False)
     print("---------")
 
