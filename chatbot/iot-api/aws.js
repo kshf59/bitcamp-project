cb0f74482dcf8e022ea8fb121a37b76345b7d341
kim <kshf59@naver.com>
2017-12-12 21:19:25 +0900

    d

--- chatbot/iot-api/aws.js
+++ chatbot/iot-api/aws.js
@@ -1,26 +1,26 @@
 const awsIot = require('aws-iot-device-sdk');
 
-// 장비 목록 
+// 장비 목록
 const devices = {};
 
 const dev01 = awsIot.device({
-    keyPath: "/home/ec2-user/vars/aws-iot/dev01/dev01.private.key", 
+    keyPath: "/home/ec2-user/vars/aws-iot/dev01/dev01.private.key",
     certPath: "/home/ec2-user/vars/aws-iot/dev01/dev01.cert.pem",
-    caPath: "/home/ec2-user/vars/aws-iot/root-CA.crt", 
-    clientId: "fbchatbot", 
-    host: "a3w0dqjvzngs31.iot.ap-northeast-2.amazonaws.com" 
+    caPath: "/home/ec2-user/vars/aws-iot/root-CA.crt",
+    clientId: "chatbot",
+    host: "a3w0dqjvzngs31.iot.ap-northeast-2.amazonaws.com"
 });
 
 dev01.on('connect', function() {
-    console.log('AWS IoT의 dev01 장비와 연결 되었음!');
+    console.log('AWS IoT의 dev01 장비와 연결되었음!');
     // 연결에 성공하면 연결된 장비를 목록에 추가한다.
     devices['dev01'] = dev01;
-});
+  });
 
 function publish(deviceName, topic, dataObj) {
-    devices[deviceName].publish(topic, JSON.stringify(dataObj));
+    devices[deviceName].publish(topic, JSON.stringify(dataObj))
 }
 
 module.exports = {
-     publish
+    publish
 };
