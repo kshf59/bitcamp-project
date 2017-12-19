8fd3653c145df0c0461a33748f1ad77a5b8050e7
kim <kshf59@naver.com>
2017-12-19 18:35:07 +0900

    d

--- iot-py/shadow.py
+++ iot-py/shadow.py
@@ -8,11 +8,18 @@ import led_api as led
 
 # shadow 명령을 실행한 후 호출될 함수 정의 
 def updateCallback(payload, responseStatus, token):
+    print("update 명령 수행 결과!-------------")
     print(payload)
     print(responseStatus)
     print(token)
     print("--------------")
 
+def getCallback(payload, responseStatus, token):
+    print("get 명령 수행 결과!-------------")
+    print(payload)
+    print(responseStatus)
+    print(token)
+    print("--------------")
 
 # AWS IoT의 Thing에 접속할 때 사용할 정보 준비
 
@@ -62,6 +69,9 @@ print("shadow connect\n")
 # thing shadow 객체 생성
 myDeviceShadow = myShadowClient.createShadowHandlerWithName("dev01", True)
 
+# 현재 shadow 값 가져오기
+myDeviceShadow.shadowGet(getCallback, 5)
+
 # shadow 값 변경하기
-myJSONPayload = '{"state":{"desired":{"led":"on"}}}'
-myDeviceShadow.shadowUpdate(myJSONPayload, updateCallback, 5)
+#myJSONPayload = '{"state":{"desired":{"led":"on"}}}'
+#myDeviceShadow.shadowUpdate(myJSONPayload, updateCallback, 5)
